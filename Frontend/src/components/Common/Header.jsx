c9b1a6565242d6693c1bf799837497e76ffb2185
Mohottige.S.D. <146794439+Sahani-Mohottige@users.noreply.github.com>
2025-05-07 18:02:06 +0530

    Create folder structure

---
+++ Frontend/src/components/Common/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import TopBar from '../Layout/TopBar'
+
+ const Header = () => {
+  return (
+ <>
+    {/*Topbar*/}
+    <TopBar/>
+   {/*Nav bar*/}
+   {/*Cart drawer*/}</>
+  )
+}
+
+export default Header
