a754b0ed71a21499d8214aea6563808f8d8dee3c
Mohottige.S.D. <146794439+Sahani-Mohottige@users.noreply.github.com>
2025-05-14 12:09:27 +0530

    complete best seller section

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,12 +2,14 @@ import {BrowserRouter, Route, Routes} from "react-router-dom"
 
 import Home from "./pages/Home";
 import React from "react";
+import {Toaster} from "sonner"
 import UserLayout from './components/Layout/UserLayout';
 
 const App= () => {
   
   return (
 <BrowserRouter>
+<Toaster position="top-right"/>
     <Routes>
         <Route path="/" element={<UserLayout/>}> 
         <Route index element={<Home/>}/> </Route>
--- Frontend/src/components/Products/ProductDetails.jsx
+++ Frontend/src/components/Products/ProductDetails.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+import {toast} from "sonner"
 
 const selectedProduct = {
   name: "Jacket",
@@ -21,8 +23,44 @@ const selectedProduct = {
   ],
 };
 
-
 const ProductDetails = () => {
+
+  const[mainImage,setMainImage] =useState("");
+  const[selectedSize,setSelectedSize] =useState("");
+  const[selectedColor,setSelectedColor] =useState("");
+  const[quantity,setQuantity] =useState(1);
+  const [isButtonDisabled,setIsButtonDisabled] = useState(false);
+
+  useEffect(()=>{
+    if(selectedProduct?.images?.length>0){
+      setMainImage(selectedProduct.images[0].url);
+    }
+  },[selectedProduct]);
+
+  const handleQuantityChange = (action) =>{
+    if(action === "plus") setQuantity((prev)=>prev +1);
+    if(action === "minus" && quantity > 1) setQuantity((prev)=>prev-1);
+  }
+
+  const handleAddToCart =()=>{
+    if(!selectedSize || !selectedColor){
+      toast.error("Please select a size and color before adding to cart.",{
+        duration :1000,
+      });
+      return;
+    }
+    
+    setIsButtonDisabled(true);
+
+    setTimeout(() =>{
+      toast.success("product added to cart!",{
+        duration:1000,
+      });
+      setIsButtonDisabled(false);
+    },500);
+    
+  };
+  
   return (
     <div className='p-6'>
       <div className='max-w-6xl mx-auto bg-white p-8 rounded-lg '>
@@ -34,7 +72,9 @@ const ProductDetails = () => {
               key={index}
               src={image.url}
               alt={image.altText||`Thumbnail ${index}`}
-              className='w-20 h-20 object-cover rounded-lg cursor-pointer border'
+              onClick={()=>setMainImage(image.url)}
+              className={`w-20 h-20 object-cover rounded-lg cursor-pointer border
+                 ${mainImage === image.url?"border-black":"border-gray-300"}`}
               />
             ))}
           </div>
@@ -42,9 +82,9 @@ const ProductDetails = () => {
           <div className="md:w-1/2">
             <div className="mb-4">
               <img 
-              src={selectedProduct.images[0]?.url}
+              src={mainImage}
               alt="Main Product"
-              className="w-full h-1500px md:h- object-cover rounded-lg"
+              className="w-full h-auto md:h- object-cover rounded-lg"
               />
             </div>
           </div>
@@ -55,7 +95,9 @@ const ProductDetails = () => {
               key={index}
               src={image.url}
               alt={image.altText||`Thumbnail ${index}`}
-              className='w-20 h-20 mb-6 object-cover rounded-lg cursor-pointer border'
+              onClick={()=>setMainImage(image.url)}
+              className={`w-20 h-20 object-cover rounded-lg cursor-pointer border
+                 ${mainImage === image.url?"border-black":"border-gray-300"}`}
               />
             ))}
            </div>
@@ -81,7 +123,9 @@ const ProductDetails = () => {
                   {selectedProduct.colors.map((color)=>(
                     <button
                     key={color}
-                    className='w-8 h-8 rounded-full'
+                    onClick={()=>setSelectedColor(color)}
+                    className={`w-8 h-8 rounded-full border 
+                      ${selectedColor === color? "border-4 border-gray-800":"border-gray-300"}`}
                     style={{
                       backgroundColor:color.toLocaleLowerCase(),
                       filter:"brightness(0.5)"
@@ -97,7 +141,9 @@ const ProductDetails = () => {
                   {selectedProduct.sizes.map((size)=>(
                     <button
                     key={size}
-                    className='px-4 py-2 rounded border'
+                    onClick={()=>setSelectedSize(size)}
+                    className={`px-4 py-2 rounded border 
+                      ${selectedSize === size? "bg-black text-white":""}`}
                     >{size}</button>
                   ))}
                 </div>
@@ -106,21 +152,23 @@ const ProductDetails = () => {
               <div className='mb-6'>
                 <p className='text-gray-700'>Quantity:</p>
                 <div className="flex items-center space-x-4 mt-2">
-                    <button className='px-2 py-1 rounded bg-gray-200 text-lg'>
+                    <button onClick={()=>handleQuantityChange("minus")} className='px-2 py-1 rounded bg-gray-200 text-lg'>
                       -
                       </button>
-                    <span className="text-lg"> 1</span>
-                     <button className='px-2 py-1 rounded bg-gray-200 text-lg'>
+                    <span className="text-lg"> {quantity}</span>
+                     <button onClick={()=>handleQuantityChange("plus")} className='px-2 py-1 rounded bg-gray-200 text-lg'>
                       +
                       </button>
                 </div>
               </div>
-              <button className="bg-black text-white py-2 px-6 rounded w-full mb-4">
-                Add to Cart
+              <button 
+              onClick={handleAddToCart}
+              className={`bg-black text-white py-2 px-6 rounded w-full mb-4 ${isButtonDisabled? "cursor-not-allowed":"hover:bg-gray-900"}`}>
+               {isButtonDisabled?"Adding...": "Add to Cart"}
                 </button>
                 
                 <div className="mt-6">
-                  <h3 className="text-xl font-semibold mb-4">Product Details</h3>
+                  <h3 className="text-xl font-semibold mb-4">Characteristics:</h3>
                   <table className="w-full text-left border border-gray-200 rounded-lg overflow-hidden">
                     <tbody>
                   <tr>
