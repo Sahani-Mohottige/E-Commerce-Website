f0c7c73b1cd6306469b2bc6806b5c2c2b7dfd92f
Mohottige.S.D. <146794439+Sahani-Mohottige@users.noreply.github.com>
2025-06-10 13:53:48 +0530

    done checkout page with paypal connection

--- Frontend/src/components/Cart/Checkout.jsx
+++ Frontend/src/components/Cart/Checkout.jsx
@@ -1,30 +1,39 @@
 import React, { useState } from 'react';
 
+import PayPalButton from './PayPalButton';
 import { useNavigate } from 'react-router-dom';
 
 const cart = {
   products: [
     {
       id: 1,
-      name: 'product1',
+      size:"M",
+      color:"Black",
+      name: "Stylish Jacket",
       price: 100,
       images: [{ url: 'https://picsum.photos/500/500?random=1' }],
     },
     {
       id: 2,
-      name: 'product1',
+      size:"L",
+      color:"Red",
+      name: "Casual Jacket",
       price: 100,
       images: [{ url: 'https://picsum.photos/500/500?random=2' }],
     },
     {
       id: 3,
-      name: 'product1',
+      size:"M",
+      color:"Black",
+      name: "Stylish Jacket",
       price: 100,
       images: [{ url: 'https://picsum.photos/500/500?random=3' }],
     },
     {
       id: 4,
-      name: 'product1',
+      size:"L",
+      color:"Red",
+      name: "Casual Jacket",
       price: 100,
       images: [{ url: 'https://picsum.photos/500/500?random=4' }],
     },
@@ -49,6 +58,11 @@ const Checkout = () => {
     e.preventDefault();
     setCheckoutId('123');
   };
+
+  const handlePaymentSuccess = (details) =>{
+    console.log("Payment Successful.",details);
+    navigate("/order-confirmation");
+  }
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 max-w-6xl mx-auto py-10 px-6">
@@ -176,14 +190,59 @@ const Checkout = () => {
               ) : (
                 <div className="p-4 border rounded bg-white shadow">
                   <h3 className="text-lg font-semibold text-green-600 mb-2">Pay with PayPal</h3>
-                  <p className="text-sm text-gray-600">Redirecting to PayPal for payment...</p>
-                  {/* You can place PayPal SDK integration or button here */}
+                  <p className="text-sm mb-6 text-gray-600">Redirecting to PayPal for payment...</p>
+                <PayPalButton amount={100} onSuccess={handlePaymentSuccess}
+                onError={(err)=>{alert("Payment Failed! Try Again.")}}/>
                 </div>
               )}
             </div>
           </div>
         </form>
       </div>
+
+        {/* Right Section */}
+      <div className="bg-blue-50 p-6 rounded-lg shadow-sm">
+        <h3 className="text-xl font-semibold mb-4">Order Summary</h3>
+        {cart.products.map((product,index) => (
+          <div 
+          key={index} 
+          className="flex items-start justify-between py-2 border-t border-blue-300"
+          >
+            <div className="flex items-start">
+            <img 
+            src={product.images[0].url} 
+            alt={product.name} 
+            className="w-20 h-20 rounded object-cover" 
+            />
+            </div>
+            <div>
+              <h3 className="text-md ">{product.name}</h3>
+              <p className=" text-gray-600">Size: {product.size}</p>
+              <p className=" text-gray-600">Color: {product.color}</p>
+            </div>
+            <div>             
+               <p className="font-bold">${product.price?.toLocaleString()}</p>
+              </div>
+          </div>
+        ))}
+        
+        <hr className="my-4 border-gray-500" />
+        <div className="flex justify-between items-center text-lg mb-4">
+          <p>Subtotal:</p>
+          <p>${cart.totalPrice?.toLocaleString()}</p>
+        </div>
+        
+        <div className="flex justify-between items-center text-lg mb-4">
+          <p>Shipping</p>
+          <p>Free</p>
+        </div>
+          <div className="flex justify-between items-center text-lg font-bold mb-4">
+          <p>Total:</p>
+          <p className="font-bold text-xl">${cart.totalPrice?.toLocaleString()}</p>
+        </div>
+        
+      </div>
+      
     </div>
   );
 };
